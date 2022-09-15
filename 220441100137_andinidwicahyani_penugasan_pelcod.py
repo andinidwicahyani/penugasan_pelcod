d99d98aa46019a233d3b7e221c2684f729ff3bfe
andinidwicahyani <113564982+andinidwicahyani@users.noreply.github.com>
2022-09-15 11:20:53 +0700

    Add files via upload

---
+++ 220441100137_andinidwicahyani_penugasan_pelcod.py
@@ -0,0 +1,12 @@
+print("Program menghitung volume balok")
+p = float(input("masukkan p :"))
+l = float(input("masukkan l : "))
+t = float(input("masukkan t : "))
+volume = p*l*t
+print(f"hasil volume balok= {volume}")
+
+
+print("Program menghitung volume kubus")
+s = float(input("masukkan s : "))
+volume = s*s*s
+print(f"hasil volume kubus= {volume}")
